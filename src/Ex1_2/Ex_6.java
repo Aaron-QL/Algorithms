6184d7b21b161d8ad30f6d4bda4374b859c8927f
秦龙 <long.qin@mfashion.com.cn>
2019-02-07 21:59:05 +0800

    Ex_6

---
+++ src/Ex1_2/Ex_6.java
@@ -0,0 +1,18 @@
+package Ex1_2;
+
+import edu.princeton.cs.algs4.StdIn;
+
+public class Ex_6 {
+    public static void main(String[] args) {
+        System.out.println("s: ");
+        String s = StdIn.readLine();
+        System.out.println("t: ");
+        String t = StdIn.readLine();
+
+        if (s.length() == t.length() && s.concat(s).contains(t)) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+    }
+}
