b517838df1acfd64f89ddb1c91a072c83f0c1839
秦龙 <long.qin@mfashion.com.cn>
2019-02-19 20:46:16 +0800

    UF

---
+++ src/Ex1_5/UFQuickFind.java
@@ -0,0 +1,42 @@
+package Ex1_5;
+
+public class UFQuickFind {
+    private int[] id;
+    private int count;
+
+    UFQuickFind(int n) {
+        count = n;
+        id = new int[n];
+        for (int i = 0; i < n; i++) {
+            id[i] = i;
+        }
+    }
+
+    public int count() {
+        return count;
+    }
+
+    public boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    public int find(int p) {
+        return id[p];
+    }
+
+    public void union(int p, int q) {
+        int pID = id[p];
+        int qID = id[q];
+
+        if (pID == qID) {
+            return;
+        }
+
+        for (int i = 0; i < count; i++) {
+            if (id[i] == pID) {
+                id[i] = qID;
+            }
+        }
+        count--;
+    }
+}
