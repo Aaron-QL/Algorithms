766b079b560cd35c75a041bdee00bd7156fc4700
akaQin <qinlong@akaqin.me>
2019-03-07 11:09:43 +0800

    quickSort

--- src/Sort/QuickSort.java
+++ src/Sort/QuickSort.java
@@ -1,7 +1,53 @@
 package Sort;
 
+
 import java.util.Arrays;
 
 public class QuickSort {
 
+    public static void main(String[] args) {
+        int[] arr = {3, 4, 1, 6, 0, 5, 7, 2};
+        sort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void sort(int[] a, int lo, int hi) {
+        if (lo >= hi) {
+            return;
+        }
+        int j = partition(a, lo, hi);
+        sort(a, lo, j - 1);
+        sort(a, j+1, hi);
+    }
+
+    private static int partition(int[] a, int lo, int hi) {
+        int i = lo, j = hi + 1, v = a[lo];
+        while (true) {
+            while (a[++i] < v) {
+                if (i == hi) {
+                    break;
+                }
+            }
+
+            while (a[--j] > v) {
+                if (j == lo) {
+                    break;
+                }
+            }
+
+            if (i >= j) {
+                break;
+            }
+
+            exch(a, i, j);
+        }
+        exch(a, lo, j);
+        return j;
+    }
+
+    private static void exch(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
 }
