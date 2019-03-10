a7d7abafb76e6d6237ced0f6fee0e379755821ac
akaQin <qinlong@akaqin.me>
2019-03-10 19:45:08 +0800

    code format

--- src/Ex2_1/Merge.java
+++ src/Ex2_1/Merge.java
@@ -18,7 +18,7 @@ public class Merge {
         sort(a, 0, a.length - 1);
     }
 
-    private static void sort(Comparable[]a, int lo, int hi) {
+    private static void sort(Comparable[] a, int lo, int hi) {
         if (hi <= lo) {
             return;
         }
@@ -29,7 +29,7 @@ public class Merge {
         merge(a, lo, mid, hi);
     }
 
-    private static void merge(Comparable[]a, int lo, int mid, int hi) {
+    private static void merge(Comparable[] a, int lo, int mid, int hi) {
         int i = lo, j = mid + 1;
         for (int k = lo; k <= hi; k++) {
             aux[k] = a[k];
@@ -38,40 +38,36 @@ public class Merge {
         for (int k = lo; k <= hi; k++) {
             if (i > mid) {
                 a[k] = aux[j++];
-            } else if(j > hi) {
+            } else if (j > hi) {
                 a[k] = aux[i++];
-            } else if(less(aux[j], aux[i])) {
-                a[k] = aux[j++];
+            } else if (less(a[i], a[j])) {
+                a[k] = aux[i++];
             } else {
-                a[k] = aux[i++];
+                a[k] = aux[j++];
             }
         }
     }
 
-    private static boolean less(Comparable v, Comparable w)
-    {
+    private static boolean less(Comparable v, Comparable w) {
         return v.compareTo(w) < 0;
     }
 
-    private static void exch(Comparable[] a, int i, int j)
-    {
+    private static void exch(Comparable[] a, int i, int j) {
         Comparable t = a[i];
         a[i] = a[j];
         a[j] = t;
     }
 
-    private static void show(Comparable[] a)
-    {
+    private static void show(Comparable[] a) {
         for (int i = 0; i < a.length; i++) {
             StdOut.print(a[i] + " ");
         }
         StdOut.println();
     }
 
-    public static boolean isSorted(Comparable[] a)
-    {
+    public static boolean isSorted(Comparable[] a) {
         for (int i = 1; i < a.length; i++) {
-            if (less(a[i], a[i-1])) {
+            if (less(a[i], a[i - 1])) {
                 return false;
             }
         }
