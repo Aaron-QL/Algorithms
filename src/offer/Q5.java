eba9124da5347e5cd59dc74faf564f68236154a1
akaQin <qinlong@akaqin.me>
2019-03-12 21:37:29 +0800

    BST

---
+++ src/offer/Q5.java
@@ -0,0 +1,28 @@
+package offer;
+
+public class Q5 {
+    public static void main(String[] args) {
+
+    }
+
+    public String replaceSpace(StringBuffer str) {
+        int p1 = str.length() - 1;
+        for (int i = 0; i <= p1; i++) {
+            if (str.charAt(i) == ' ') {
+                str.append("  ");
+            }
+        }
+        int p2 = str.length() - 1;
+        while (p1 >0 && p1 < p2) {
+            char c = str.charAt(p1--);
+            if (c == ' ') {
+                str.setCharAt(p2--, '0');
+                str.setCharAt(p2--, '2');
+                str.setCharAt(p2--, '%');
+            } else {
+                str.setCharAt(p2--, c);
+            }
+        }
+        return str.toString();
+    }
+}
