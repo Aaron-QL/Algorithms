5b62bc0160cb2ce3564c01d673ff79bace0f3687
akaQin <qinlong@akaqin.me>
2020-03-05 19:28:24 +0800

    BinarySearch

--- src/Ex1_1/BinarySearch.java
+++ src/Ex1_1/BinarySearch.java
@@ -7,28 +7,34 @@ import edu.princeton.cs.algs4.StdOut;
 import java.util.Arrays;
 
 public class BinarySearch {
-    public static int rank(int key, int[] a) {
+    private BinarySearch() { }
+
+    public static int indexOf(int[] a, int key) {
         int lo = 0;
         int hi = a.length - 1;
         while (lo <= hi) {
+            // Key is in a[lo..hi] or not present.
             int mid = lo + (hi - lo) / 2;
-            if (key < a[mid]) {
-                hi = mid - 1;
-            } else  if (key > a[mid]) {
-                lo = mid + 1;
-            } else {
-                return mid;
-            }
+            if      (key < a[mid]) hi = mid - 1;
+            else if (key > a[mid]) lo = mid + 1;
+            else return mid;
         }
         return -1;
     }
 
+    @Deprecated
+    public static int rank(int key, int[] a) {
+        return indexOf(a, key);
+    }
+
     public static void main(String[] args) {
-        int[] whitelist = In.readInts(args[0]);
+        In in = new In(args[0]);
+        int[] whitelist = in.readAllInts();
+
         Arrays.sort(whitelist);
         while (!StdIn.isEmpty()) {
             int key = StdIn.readInt();
-            if (rank(key, whitelist) < 0) {
+            if (BinarySearch.indexOf(whitelist, key) == -1) {
                 StdOut.println(key);
             }
         }
