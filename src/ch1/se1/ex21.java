e502215f4b2fecb06b01df124b12d3c11f405c47
akaQin <qinlong@akaqin.me>
2020-03-12 12:08:31 +0800

    bug fix

--- src/ch1/se1/ex21.java
+++ src/ch1/se1/ex21.java
@@ -1,20 +1,22 @@
 package ch1.se1;
 
-import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
 public class ex21 {
     public static void main(String[] args) {
-        String name;
-        int score1;
-        int score2;
-        String table = "";
-        while (!StdIn.isEmpty()) {
-            name = StdIn.readString();
-            score1 = StdIn.readInt();
-            score2 = StdIn.readInt();
-            table += String.format("%s: %d %d %d\n", name, score1, score2 , score1 / score2);
+        In in = new In();
+        while (in.hasNextLine()) {
+            String line = in.readLine();
+            String[] values = line.split(" ");
+            formatedPrint(values);
         }
-        StdOut.print(table);
+    }
+
+    private static void formatedPrint(String[] values)
+    {
+        StdOut.printf("%8s: %8s %8s ", values[0], values[1], values[2]);
+        double result = Double.parseDouble(values[1]) / Double.parseDouble(values[2]);
+        StdOut.printf("%7.3f\n", result);
     }
 }
