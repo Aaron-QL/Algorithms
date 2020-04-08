13ebdaba1af694146e57745ab14d33419533ba3f
akaQin <qinlong@akaqin.me>
2020-04-08 21:54:49 +0800

    quickUnionFind

---
+++ src/ch1/se5/QuickFindUF.java
@@ -0,0 +1,69 @@
+package ch1.se5;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class QuickFindUF {
+    private int[] id;
+    private int count;
+
+    public QuickFindUF(int n) {
+        count = n;
+        id = new int[n];
+        for (int i = 0; i < n; i++) {
+            id[i] = i;
+        }
+    }
+
+    public int count() {
+        return count;
+    }
+
+    // validate that p is a valid index
+    private void validate(int p) {
+        if (p < 0 || p >= id.length) {
+            throw new IllegalArgumentException("index " + p + " is not between 0 and " + (id.length - 1));
+        }
+    }
+
+    public int find(int p)
+    {
+        validate(p);
+        return id[p];
+    }
+
+    public boolean connected(int p, int q)
+    {
+        validate(p);
+        validate(q);
+        return find(p) == find(q);
+    }
+
+    public void union(int p, int q)
+    {
+        int pID = find(p);
+        int qID = find(q);
+        if (pID == qID) {
+            return;
+        }
+        for (int i = 0; i < id.length; i++) {
+            if (id[i] == pID) {
+                id[i] = qID;
+            }
+        }
+        count--;
+    }
+
+    public static void main(String[] args) {
+        int n = StdIn.readInt();
+        QuickFindUF uf = new QuickFindUF(n);
+        while (!StdIn.isEmpty()) {
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            if (uf.find(p) == uf.find(q)) continue;
+            uf.union(p, q);
+            StdOut.println(p + " " + q);
+        }
+        StdOut.println(uf.count() + " components");
+    }
+}
