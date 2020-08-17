3b0499c2c5cbd352fde1df659d780a515187854d
akaQin <qinlong@akaqin.me>
2020-08-17 16:30:33 +0800

    bfs

---
+++ src/ch4/BreadthFirstPaths.java
@@ -0,0 +1,140 @@
+package ch4;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
+
+public class BreadthFirstPaths {
+    private static final int INFINITY = Integer.MAX_VALUE;
+    private boolean[] marked;
+    private int[] edgeTo;
+    private int[] distTo;
+
+    public BreadthFirstPaths(Graph G, int s) {
+        this.marked = new boolean[G.V()];
+        this.edgeTo = new int[G.V()];
+        this.distTo = new int[G.V()];
+        this.validateVertex(s);
+        this.bfs(G, s);
+    }
+
+    private void bfs(Graph G, int s) {
+        Queue<Integer> q = new Queue<Integer>();
+        for (int v = 0; v < G.V(); v++) {
+            distTo[v] = INFINITY;
+        }
+        distTo[s] = 0;
+        marked[s] = true;
+        q.enqueue(s);
+        while (!q.isEmpty()) {
+            int v = q.dequeue();
+            for (int w : G.adj(v)) {
+                if (!marked[w]) {
+                    marked[w] = true;
+                    edgeTo[w] = v;
+                    distTo[w] = distTo[v] + 1;
+                    q.enqueue(w);
+                }
+            }
+        }
+    }
+
+    public int distTo(int v) {
+        this.validateVertex(v);
+        return distTo[v];
+    }
+
+    private void validateVertex(int v) {
+        int V = marked.length;
+        if (v < 0 || v >= V) {
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+        }
+    }
+
+    public boolean hasPathTo(int v) {
+        this.validateVertex(v);
+        return marked[v];
+    }
+
+    public Iterable<Integer> pathTo(int v) {
+        this.validateVertex(v);
+        if (!this.hasPathTo(v)) {
+            return null;
+        }
+        Stack<Integer> path = new Stack<Integer>();
+        int x = v;
+        for (; distTo[x] != 0; x = edgeTo[x]) {
+            path.push(x);
+        }
+        path.push(x);
+        return path;
+    }
+
+    private boolean check(Graph G, int s) {
+
+        // check that the distance of s = 0
+        if (distTo[s] != 0) {
+            StdOut.println("distance of source " + s + " to itself = " + distTo[s]);
+            return false;
+        }
+
+        // check that for each edge v-w dist[w] <= dist[v] + 1
+        // provided v is reachable from s
+        for (int v = 0; v < G.V(); v++) {
+            for (int w : G.adj(v)) {
+                if (hasPathTo(v) != hasPathTo(w)) {
+                    StdOut.println("edge " + v + "-" + w);
+                    StdOut.println("hasPathTo(" + v + ") = " + hasPathTo(v));
+                    StdOut.println("hasPathTo(" + w + ") = " + hasPathTo(w));
+                    return false;
+                }
+                if (hasPathTo(v) && (distTo[w] > distTo[v] + 1)) {
+                    StdOut.println("edge " + v + "-" + w);
+                    StdOut.println("distTo[" + v + "] = " + distTo[v]);
+                    StdOut.println("distTo[" + w + "] = " + distTo[w]);
+                    return false;
+                }
+            }
+        }
+
+        // check that v = edgeTo[w] satisfies distTo[w] = distTo[v] + 1
+        // provided v is reachable from s
+        for (int w = 0; w < G.V(); w++) {
+            if (!hasPathTo(w) || w == s) continue;
+            int v = edgeTo[w];
+            if (distTo[w] != distTo[v] + 1) {
+                StdOut.println("shortest path edge " + v + "-" + w);
+                StdOut.println("distTo[" + v + "] = " + distTo[v]);
+                StdOut.println("distTo[" + w + "] = " + distTo[w]);
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        Graph G = new Graph(in);
+        // StdOut.println(G);
+
+        int s = Integer.parseInt(args[1]);
+        BreadthFirstPaths bfs = new BreadthFirstPaths(G, s);
+
+        for (int v = 0; v < G.V(); v++) {
+            if (bfs.hasPathTo(v)) {
+                StdOut.printf("%d to %d (%d):  ", s, v, bfs.distTo(v));
+                for (int x : bfs.pathTo(v)) {
+                    if (x == s) StdOut.print(x);
+                    else        StdOut.print("-" + x);
+                }
+                StdOut.println();
+            }
+
+            else {
+                StdOut.printf("%d to %d (-):  not connected\n", s, v);
+            }
+        }
+    }
+}
