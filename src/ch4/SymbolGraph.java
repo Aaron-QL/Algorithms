07de0151616cc1eae26ab1a6f9a5052888c2c8f7
akaQin <qinlong@akaqin.me>
2020-08-18 17:23:21 +0800

    Symbol Graph

--- src/ch4/SymbolGraph.java
+++ src/ch4/SymbolGraph.java
@@ -2,6 +2,8 @@ package ch4;
 
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.ST;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
 
 public class SymbolGraph {
     private ST<String, Integer> st;
@@ -48,7 +50,65 @@ public class SymbolGraph {
         return this.keys[v];
     }
 
-    public Graph G() {
-        return this.graph;
+    /**
+     * Returns the integer associated with the vertex named {@code s}.
+     * @param s the name of a vertex
+     * @return the integer (between 0 and <em>V</em> - 1) associated with the vertex named {@code s}
+     */
+    public int indexOf(String s) {
+        return st.get(s);
+    }
+
+    /**
+     * Returns the name of the vertex associated with the integer {@code v}.
+     * @param  v the integer corresponding to a vertex (between 0 and <em>V</em> - 1)
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     * @return the name of the vertex associated with the integer {@code v}
+     */
+    public String nameOf(int v) {
+        validateVertex(v);
+        return keys[v];
+    }
+
+
+    /**
+     * Returns the graph assoicated with the symbol graph. It is the client's responsibility
+     * not to mutate the graph.
+     * @return the graph associated with the symbol graph
+     */
+    public Graph graph() {
+        return graph;
+    }
+
+    // throw an IllegalArgumentException unless {@code 0 <= v < V}
+    private void validateVertex(int v) {
+        int V = graph.V();
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+    }
+
+
+    /**
+     * Unit tests the {@code SymbolGraph} data type.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        String filename  = args[0];
+        String delimiter = args[1];
+        SymbolGraph sg = new SymbolGraph(filename, delimiter);
+        Graph graph = sg.graph();
+        while (StdIn.hasNextLine()) {
+            String source = StdIn.readLine();
+            if (sg.contains(source)) {
+                int s = sg.index(source);
+                for (int v : graph.adj(s)) {
+                    StdOut.println("   " + sg.name(v));
+                }
+            }
+            else {
+                StdOut.println("input not contain '" + source + "'");
+            }
+        }
     }
 }
