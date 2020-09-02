fd1f3dd737f901340c9d9c4989bedee92f3dea87
akaQin <qinlong@akaqin.me>
2020-09-02 10:24:52 +0800

    digraph

---
+++ src/ch4/Digraph.java
@@ -0,0 +1,139 @@
+package ch4;
+
+import ch1.se3.Bag;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.NoSuchElementException;
+
+@SuppressWarnings("unchecked")
+public class Digraph {
+    private static final String NEWLINE = System.getProperty("line.separator");
+    private final int V;
+    private int E;
+    private Bag<Integer>[] adj;
+    private int[] indegree;
+
+    public Digraph(int v) {
+        if (v < 0) {
+            throw new IllegalArgumentException();
+        }
+        this.V = v;
+        this.E = 0;
+        this.adj = (Bag<Integer>[]) new Bag[v];
+        this.indegree = new int[v];
+        for (int i = 0; i < v; i++) {
+            this.adj[i] = new Bag<Integer>();
+        }
+    }
+
+    public Digraph(In in) {
+        if (in == null) {
+            throw new IllegalArgumentException();
+        }
+        try {
+            this.V = in.readInt();
+            if (this.V < 0) {
+                throw new IllegalArgumentException();
+            }
+            this.adj = (Bag<Integer>[]) new Bag[this.V];
+            this.indegree = new int[this.V];
+            for (int i = 0; i < this.V; i++) {
+                this.adj[i] = new Bag<Integer>();
+            }
+            int E = in.readInt();
+            if (E < 0) {
+                throw new IllegalArgumentException();
+            }
+            for (int i = 0; i < E; i++) {
+                int v = in.readInt();
+                int w = in.readInt();
+                this.addEdge(v, w);
+            }
+        } catch (NoSuchElementException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void addEdge(int v, int w) {
+        this.validateVertex(v);
+        this.validateVertex(w);
+        this.adj[v].add(w);
+        this.indegree[w]++;
+        this.E++;
+    }
+
+    private void validateVertex(int v) {
+        if (v < 0 || v >= this.V) {
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+        }
+    }
+
+    public int V() {
+        return this.V;
+    }
+
+    public int E() {
+        return this.E;
+    }
+
+    public Iterable<Integer> adj(int v) {
+        this.validateVertex(v);
+        return this.adj[v];
+    }
+
+    public int outDegree(int v) {
+        this.validateVertex(v);
+        return this.adj[v].size();
+    }
+
+    public int inDegree(int v) {
+        this.validateVertex(v);
+        return this.indegree[v];
+    }
+
+    /**
+     * Returns the reverse of the digraph.
+     *
+     * @return the reverse of the digraph
+     */
+    public Digraph reverse() {
+        Digraph reverse = new Digraph(V);
+        for (int v = 0; v < V; v++) {
+            for (int w : adj(v)) {
+                reverse.addEdge(w, v);
+            }
+        }
+        return reverse;
+    }
+
+    /**
+     * Returns a string representation of the graph.
+     *
+     * @return the number of vertices <em>V</em>, followed by the number of edges <em>E</em>,
+     *         followed by the <em>V</em> adjacency lists
+     */
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append(V + " vertices, " + E + " edges " + NEWLINE);
+        for (int v = 0; v < V; v++) {
+            s.append(String.format("%d: ", v));
+            for (int w : adj[v]) {
+                s.append(String.format("%d ", w));
+            }
+            s.append(NEWLINE);
+        }
+        return s.toString();
+    }
+
+    /**
+     * Unit tests the {@code Digraph} data type.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        Digraph G = new Digraph(in);
+        StdOut.println(G);
+    }
+}
---
+++ src/ch4/DirectedDFS.java
@@ -0,0 +1,105 @@
+package ch4;
+
+import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+public class DirectedDFS {
+    private boolean[] marked;
+    private int count;
+
+    public DirectedDFS(Digraph G, Iterable<Integer> sources) {
+        marked = new boolean[G.V()];
+        validateVertices(sources);
+        for (int v : sources) {
+            if (!marked[v]) dfs(G, v);
+        }
+    }
+
+    public DirectedDFS(Digraph G, int s) {
+        this.marked = new boolean[G.V()];
+        this.validateVertex(s);
+        this.dfs(G, s);
+    }
+
+    private void dfs(Digraph G, int v) {
+        this.marked[v] = true;
+        this.count++;
+        for (int w : G.adj(v)) {
+            if (!this.marked[w]) {
+                this.dfs(G, w);
+            }
+        }
+    }
+
+    /**
+     * Is there a directed path from the source vertex (or any
+     * of the source vertices) and vertex {@code v}?
+     * @param  v the vertex
+     * @return {@code true} if there is a directed path, {@code false} otherwise
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    public boolean marked(int v) {
+        validateVertex(v);
+        return marked[v];
+    }
+
+    /**
+     * Returns the number of vertices reachable from the source vertex
+     * (or source vertices).
+     * @return the number of vertices reachable from the source vertex
+     *   (or source vertices)
+     */
+    public int count() {
+        return count;
+    }
+
+    // throw an IllegalArgumentException unless {@code 0 <= v < V}
+    private void validateVertex(int v) {
+        int V = marked.length;
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+    }
+
+    // throw an IllegalArgumentException unless {@code 0 <= v < V}
+    private void validateVertices(Iterable<Integer> vertices) {
+        if (vertices == null) {
+            throw new IllegalArgumentException("argument is null");
+        }
+        for (Integer v : vertices) {
+            if (v == null) {
+                throw new IllegalArgumentException("vertex is null");
+            }
+            validateVertex(v);
+        }
+    }
+
+
+    /**
+     * Unit tests the {@code DirectedDFS} data type.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+
+        // read in digraph from command-line argument
+        In in = new In(args[0]);
+        Digraph G = new Digraph(in);
+
+        // read in sources from command-line arguments
+        Bag<Integer> sources = new Bag<Integer>();
+        for (int i = 1; i < args.length; i++) {
+            int s = Integer.parseInt(args[i]);
+            sources.add(s);
+        }
+
+        // multiple-source reachability
+        DirectedDFS dfs = new DirectedDFS(G, sources);
+
+        // print out vertices reachable from sources
+        for (int v = 0; v < G.V(); v++) {
+            if (dfs.marked(v)) StdOut.print(v + " ");
+        }
+        StdOut.println();
+    }
+}
